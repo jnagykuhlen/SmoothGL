d98c8840aa3df83fc6ec3043912f2c3eac6496dd
Jonas Nagy-Kuhlen <jonasnagykuhlen@aol.com>
2024-12-23 11:17:45 +0100

    Flip textures vertically on read/write for compatibility with OpenGL coordinate system

--- SmoothGL/Graphics/Texturing/ImageData.cs
+++ SmoothGL/Graphics/Texturing/ImageData.cs
@@ -10,6 +10,12 @@ namespace SmoothGL.Graphics.Texturing;
 /// </summary>
 public class ImageData
 {
+    static ImageData()
+    {
+        StbImage.stbi_set_flip_vertically_on_load(1);
+        StbImageWrite.stbi_flip_vertically_on_write(1);
+    }
+    
     /// <summary>
     /// Creates new image data in client memory.
     /// </summary>
@@ -41,6 +47,12 @@ public class ImageData
     /// </summary>
     public byte[] Data { get; }
 
+    /// <summary>
+    /// Gets a rectangular section of this image data as a new image data object.
+    /// </summary>
+    /// <param name="rectangle">Rectangle defining the section to copy.</param>
+    /// <returns>Section of this image data.</returns>
+    /// <exception cref="ArgumentException">Section is not within bounds of this image data.</exception>
     public ImageData GetSection(Rectangle rectangle)
     {
         if (rectangle.X < 0 || rectangle.Y < 0 || rectangle.Width < 0 || rectangle.Height < 0 ||
@@ -48,27 +60,35 @@ public class ImageData
             throw new ArgumentException("The provided rectangle is out of bounds.", nameof(rectangle));
 
         var sectionData = new byte[4 * rectangle.Width * rectangle.Height];
-        for (int y = 0; y < rectangle.Height; ++y)
-        {
+        for (var y = 0; y < rectangle.Height; ++y)
             Array.Copy(Data, 4 * ((rectangle.Y + y) * Width + rectangle.X), sectionData, 4 * y * rectangle.Width, 4 * rectangle.Width);
-        }
 
         return new ImageData(rectangle.Width, rectangle.Height, sectionData);
     }
 
+    /// <summary>
+    /// Load image data from a PNG or JPG stream.
+    /// </summary>
+    /// <param name="stream">Stream containing the image data.</param>
+    /// <returns>Image data loaded from the stream.</returns>
     public static ImageData FromStream(Stream stream)
     {
         var imageResult = ImageResult.FromStream(stream, ColorComponents.RedGreenBlueAlpha);
         return new ImageData(imageResult.Width, imageResult.Height, imageResult.Data);
     }
 
-    public void WritePng(Stream stream)
-    {
+    /// <summary>
+    /// Writes this image data to a PNG stream.
+    /// </summary>
+    /// <param name="stream">Stream to which the image data is written.</param>
+    public void WritePng(Stream stream) =>
         new ImageWriter().WritePng(Data, Width, Height, StbImageWriteSharp.ColorComponents.RedGreenBlueAlpha, stream);
-    }
 
-    public void WriteJpg(Stream stream, int quality)
-    {
+    /// <summary>
+    /// Writes this image data to a JPG stream.
+    /// </summary>
+    /// <param name="stream">Stream to which the image data is written.</param>
+    /// <param name="quality">Quality of the JPG image, in the range of 1 to 100.</param>
+    public void WriteJpg(Stream stream, int quality) =>
         new ImageWriter().WriteJpg(Data, Width, Height, StbImageWriteSharp.ColorComponents.RedGreenBlueAlpha, stream, quality);
-    }
 }
